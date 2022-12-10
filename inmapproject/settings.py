2ed7b5a31454aff8a81996b99797c75a8468820d
SOORAJTS2001 <cherrycheeks404@gmail.com>
2022-12-10 18:27:09 +0530

    first commit

--- inmapproject/settings.py
+++ inmapproject/settings.py
@@ -136,3 +136,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://web-production-6ec7.up.railway.app/']
